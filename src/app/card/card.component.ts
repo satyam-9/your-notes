db6fd3906e82cc25114f9235c9bb7a3eb829878d
Satyam <satyam8anand2@gmail.com>
2024-12-17 15:57:03 +0530

    initial commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() pdfUrl: string = '';
+}
